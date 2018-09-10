6b9d24bc644a35c2ee742758af505b71e466f448
jsjtwsh <875686769@qq.com>
2018-09-10 23:29:03 +0800

    项目初始化

---
+++ src/components/header/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="header">
+     我是 header
+   </div>
+   <!-- <div class="tab">
+     I an tab
+   </div>
+   <div>
+     I am content
+   </div> -->
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default{}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
